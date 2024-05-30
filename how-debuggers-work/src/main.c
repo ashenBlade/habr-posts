33d68a694caa887cfe507bc857afa22dabe825dc
ash.blade <ashen.blade.dev@gmail.com>
2024-05-30 19:49:30 +0300

    рабочие tracer и tracee

--- how-debuggers-work/src/main.c
+++ how-debuggers-work/src/main.c
@@ -1,14 +1,17 @@
 #include <sys/ptrace.h>
-#include <wait.h>
+#include <sys/types.h>
 #include <sys/user.h>
+#include <sys/wait.h>
+#include <wait.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <errno.h>
+#include <string.h>
+#include <fcntl.h>
 
 static void tracee_main(int argc, char **argv)
 {
@@ -23,32 +26,80 @@ static void tracee_main(int argc, char **argv)
     exit(EXIT_FAILURE);
 }
 
+static const char *rbp_addr_filename = "./rbp_addr_test";
+
+static int
+obtain_rbp_address(long *addr)
+{
+    int fd = open(rbp_addr_filename, O_RDONLY);
+    if (fd == -1)
+    {
+        if (errno == ENOENT)
+        {
+            return -1;
+        }
+
+        perror("open");
+        exit(1);
+    }
+
+    *addr = 0;
+    if (read(fd, (void *)addr, sizeof(long)) == -1)
+    {
+        perror("read");
+        exit(1);
+    }
+
+    if (close(fd) == -1)
+    {
+        perror("close");
+        exit(1);
+    }
+
+    if (unlink(rbp_addr_filename) == -1)
+    {
+        perror("unlink");
+        exit(1);
+    }
+
+    return 0;
+}
+
 static int perform_action(pid_t child_pid)
 {
-    struct user_regs_struct regs;
-    if (ptrace(PTRACE_GETREGS, child_pid, NULL, &regs) == -1)
+    /*
+     * Stack:
+     * | ....  <--- %rbp
+     * | left  <--- %rbp - 8
+     * | right <--- %rbp - 16
+     * | sum   <--- %rbp - 24
+     */
+    long rbp_addr;
+    if (obtain_rbp_address(&rbp_addr) == -1)
     {
-        perror("ptrace");
+        return 0;
+    }
+
+    long right_addr = rbp_addr - 16;
+    errno = 0;
+    long right_data = ptrace(PTRACE_PEEKDATA, child_pid, (void *)right_addr, NULL);
+    if (errno != 0)
+    {
+        perror("ptrace(1)");
         return -1;
     }
 
-    // TODO: поменять rax и rbx на другое что-нибудь
-    if (regs.rax == 0)
+    if (right_data != 3)
     {
-        void *addr = (void *)regs.rbx;
-        void *data;
-        if (ptrace(PTRACE_PEEKTEXT, child_pid, addr, (void *)&data) == -1)
-        {
-            perror("ptrace");
-            return -1;
-        }
+        return 0;
+    }
 
-        *((int *)&data) = 1;
-        if (ptrace(PTRACE_POKETEXT, child_pid, addr, (void *)&data) == -1)
-        {
-            perror("ptrace");
-            return -1;
-        }
+    right_data = 0;
+
+    if (ptrace(PTRACE_POKEDATA, child_pid, (void *)right_addr, (void *)right_data) == -1)
+    {
+        perror("ptrace(2)");
+        return -1;
     }
 
     return 0;
@@ -83,11 +134,12 @@ static void tracer_main(pid_t child_pid)
 
         if (ptrace(PTRACE_CONT, child_pid, NULL, NULL) == -1)
         {
+            perror("ptrace");
             break;
         }
     }
 
-    if (kill(SIGKILL, child_pid) == -1 && errno != ESRCH)
+    if (kill(child_pid, SIGKILL) == -1 && errno != ESRCH)
     {
         perror("kill");
         exit(EXIT_FAILURE);
@@ -96,7 +148,6 @@ static void tracer_main(pid_t child_pid)
     if (waitpid(child_pid, &wstatus, 0) == -1)
     {
         perror("waitpid");
-        exit(EXIT_FAILURE);
     }
 
     exit(EXIT_FAILURE);
@@ -106,7 +157,7 @@ int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        printf("Usage: %s PROGRAM [ARGS]\n", argv[0]);
+        printf("Usage: %s PROGRAM [ARGS ...]\n", argv[0]);
         exit(1);
     }
 
@@ -120,11 +171,11 @@ int main(int argc, char **argv)
     if (pid == 0)
     {
         tracee_main(argc, argv);
-        return 1;
     }
     else
     {
         tracer_main(pid);
-        return 0;
     }
+
+    return 1;
 }
--- how-debuggers-work/src/tracee.s
+++ how-debuggers-work/src/tracee.s
@@ -1,25 +1,18 @@
 .globl main
 
-.data
+.section .data
 # const char* message = ...
 message: .asciz "%ld + %ld = %ld\n"
+.balign 8
+# const char *rbp_filename = ...
+rbp_filename: .asciz "./rbp_addr_test"
 
-.text
+.section .text
+
 # int main(int argc, const char** argv)
 main:
-    # Alignment 
-    subq $8, %rsp
-    
-    pushq %rdi
-    pushq %rsi
-
-    movq $5, %rdi
-    movq $1, %rsi
-    movq $48, %rax
-    syscall
-
-    popq %rsi
-    popq %rdi
+    pushq %rbp
+    movq %rsp, %rbp
 
     # \if (argc != 3) return 1;
     movq %rdi, %rax
@@ -53,8 +46,8 @@ main:
     pushq %rsi
     pushq %rdi
 
-    movq 16(%rsp), %rdi
-    movq 24(%rsp), %rsi
+    movq -8(%rbp), %rdi
+    movq -16(%rbp), %rsi
     call sum
 
     popq %rdi
@@ -66,24 +59,31 @@ main:
 
     # In signal handler, stack will look like this
     # | ..... |
-    # |-------|
+    # |-------| <-------- %rbp
     # | left  |
-    # |-------| <-------- %rsp - 24
+    # |-------| <-------- %rbp - 8
     # | right |
-    # |-------| <-------- %rsp - 16
+    # |-------| <-------- %rbp - 16
     # |  sum  |
-    # |-------| <-------- %rsp - 8
+    # |-------| <-------- %rbp - 24
     # | %rdi  |
-    # |-------| <-------- %rsp
+    # |-------| <-------- %rbp - 32
     # 
     # We use SIGCHLD, because it ignored by default
 
+    # dump_rbp_address(%rbp)
     pushq %rdi
+
+    movq %rbp, %rdi
+    call dump_rbp_address
+    
+    # raise(SIGCHLD)
     movq $17, %rdi
     call raise
+
     popq %rdi
 
-    # printf(message, left, right, result)
+    # printf(message, left, right, sum)
     leaq message(%rip), %rdi
     popq %rcx # sum
     popq %rdx # right
@@ -91,12 +91,12 @@ main:
     call printf
 
     # return 0
-    addq $8, %rsp
+    popq %rbp
     movq $0, %rax
     ret
 
 main_error_exit:
-    addq $8, %rsp
+    popq %rbp
     movq $1, %rax
     ret
 
@@ -127,6 +127,71 @@ parse_number:
     popq %rsi
     popq %rdi
 
+    # Это зачем?
     movq %rbp, %rsp
     popq %rbp
     ret
+
+# void dump_rbp_address(unsigned long saved_rbp)
+dump_rbp_address:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    # long saved_rbp -8(%rbp)
+    # int fd         -16(%rbp)
+    subq $16, %rsp
+    movq %rdi, -8(%rbp)
+
+    # fd = open(rbp_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+    pushq %rdi
+    pushq %rsi
+    pushq %rdx
+
+    movq $0, %rax
+    leaq rbp_filename(%rip), %rdi
+    movq $577, %rsi
+    movq $436, %rdx
+    call open
+    movl %eax, -16(%rbp)
+
+    popq %rdx
+    popq %rsi
+    popq %rdi
+
+    # \if (fd == -1) exit(1)
+    cmpl $-1, %eax
+    jz dump_rbp_address_fail
+    movl %eax, -16(%rbp)
+    
+    # \if (write(fd, (void*)&saved_rbp, sizeof(long)) == -1) exit(1)
+    pushq %rdi
+    pushq %rsi
+    pushq %rdx
+
+    movl -16(%rbp), %edi
+    movq %rbp, %rsi
+    subq $8, %rsi
+    movq $8, %rdx
+    call write
+    cmpl $-1, %eax
+    jz dump_rbp_address_fail
+
+    popq %rdx
+    popq %rsi
+    popq %rdi
+
+    # \if (close(fd) == -1) exit(1)
+    movl -16(%rbp), %edi
+    call close
+    cmpl $-1, %eax
+    jz dump_rbp_address_fail
+
+    addq $16, %rsp
+    movq %rbp, %rsp
+    popq %rbp
+    ret
+    
+    # exit(1)
+dump_rbp_address_fail:
+    movq $2, %rdi
+    call exit
